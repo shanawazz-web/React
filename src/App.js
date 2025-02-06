2d2957d34b9a4cb1fb9535f29b8b0452a322e7a1
shanawazz-dev <shanawazz-dev>
2025-02-06 12:57:49 +0530

    States

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
-// ! COmponent composition
+// ! Component composition
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import Main from './components/Main'
+// import Sidebar1 from './components/Sidebar1'
+// import Sidebar2 from '/components/Sidebar2'
+// import Footer from './components/Footer'
+// const App = () => {
+//   return (
+//     <div classname='app'>
+//       <Navbar/>
+//       <Main/>
+//       <div classname='sidebar'>
+//         <Sidebar1/>
+//         <Sidebar2/>
+//       </div>
+//       <Footer/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// !props
+
+// import React, {Component} from 'react'
+// import FBCPropEx from './propsex/FBCPropEx'
+// // import CBCPropEx from './propsex/CBCPropEx'
+
+// export default class App extends Component {
+//   render(){
+//     return(
+//       <div>
+//         {/* <CBCPropEx
+//         username="shanawaz"
+//         age={20}
+//         hobbies={["gaming" , "gym"]}
+//         address={{city:"karimnagar" , area:"kashmeergadda"}}
+//         sendFun={function(){alert("hi i am shanawaz")}}
+//         /> */}
+
+//         <FBCPropEx
+//         username="Abhinay"
+//         isMarried={true}
+//         hobbies={["chatting" , "Eating" , "Sleeping"]}
+//         />
+//       </div>
+//     )
+//   }
+// }
+  
+// ! Prop children
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+// ! state example
+
 import React from 'react'
-import Navbar from './components/Navbar'
-import Main from './components/Main'
-import Sidebar1 from './components/Sidebar1'
-import Sidebar2 from '/components/Sidebar2'
-import Footer from './components/Footer'
+import CBCStateEx from './stateexample/CBCStateEx'
+import FBCStateEx from './stateexample/FBCStateEx'
+
 const App = () => {
   return (
-    <div classname='app'>
-      <Navbar/>
-      <Main/>
-      <div classname='sidebar'>
-        <Sidebar1/>
-        <Sidebar2/>
-      </div>
-      <Footer/>
+    <div>
+      <CBCStateEx/>
+      <FBCStateEx/>
     </div>
   )
 }
 
 export default App
+
---
+++ src/stateexample/CBCStateEx.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class CBCStateEx extends Component {
+    constructor(){
+        super();
+        this.state={
+            users:["shanawaz"]
+        };
+    }
+    changeNumber=()=>{
+        this.setState({num:200})
+    }
+  render() {
+    console.log(this);
+    return (
+      <div>
+        {
+            this.state.users.map((user , i)=>{
+                return <li key={i}>{user}</li>
+            })
+        }
+        <h1>{this.state.num}</h1>
+        <button onClick={this.changeNumber}>Change Number</button>
+      </div>
+    )
+  }
+}
+export default CBCStateEx;
